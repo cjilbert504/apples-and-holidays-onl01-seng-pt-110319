7a2486e78c26530c78a0ba64b1e8e7e3c4b82d12
Collin Jilbert <cjilbert504@gmail.com>
2019-11-15 16:34:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |key, value|
-  key.capitalize
+  .capitalize
   puts "#{key}: #{value}"
   end
 end
