38d17fca128545e29a6e584159708a871cf211d1
Collin Jilbert <cjilbert504@gmail.com>
2019-11-15 16:45:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}"
     holiday.each do |holiday, item|
-    puts "  #{holiday.to_s}
+    puts "#{holiday.to_s}"
     end
     end
   end
